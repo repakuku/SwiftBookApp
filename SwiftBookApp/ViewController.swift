c0b24b2cdf50ba879937eb302173f7b2f285f102
Alexey Turulin <repakuku@icloud.com>
2023-08-29 16:36:02 -0400

    initial commit

--- SwiftBookApp/ViewController.swift
+++ SwiftBookApp/ViewController.swift
@@ -7,13 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+final class ViewController: UIViewController {
 
 }
 
