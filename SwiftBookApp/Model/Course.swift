eb16d713e355a6f0566a7b8b4a243102a6820d14
Alexey Turulin <repakuku@icloud.com>
2023-08-31 14:28:38 -0400

    added course model

---
+++ SwiftBookApp/Model/Course.swift
@@ -0,0 +1,15 @@
+//
+//  Course.swift
+//  SwiftBookApp
+//
+//  Created by Alexey Turulin on 8/31/23.
+//
+
+import Foundation
+
+struct Course {
+    let name: String
+    let imageUrl: URL
+    let numberOfLessons: Int
+    let numberOfTests: Int
+}
