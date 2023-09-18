8a8ba4887c7d0d46a50904d6f10a9cffae057d97
Alexey Turulin <repakuku@icloud.com>
2023-09-18 18:04:26 -0400

    working with Box

--- SwiftBookApp/Helpers/Box.swift
+++ SwiftBookApp/Helpers/Box.swift
@@ -8,9 +8,22 @@
 import Foundation
 
 final class Box<T> {
-    var value: T
+    typealias Listener = (T) -> Void
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    private var listener: Listener?
     
     init(_ value: T) {
         self.value = value
+    }
+    
+    func bind(listener: @escaping Listener) {
+        self.listener = listener
+        listener(value)
     }
 }
